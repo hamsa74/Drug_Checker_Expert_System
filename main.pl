d51d6ae3bba82873fb32de2a617a2c6136eab7ee
HamsaAboelghit <139865253+hamsa74@users.noreply.github.com>
2026-05-02 19:31:44 +0300

    Completed the Expert Knowledge

---
+++ main.pl
@@ -0,0 +1,7 @@
+/* 
+   Project: Drug Interaction Checker (AI202)
+   Team Members:
+   1- Hamsa Mustafa Aboelghit - ID: 202400350 B07
+   2- Yasmen Abdelaziz Ramadan Elqady - ID: 202400477 B09
+   
+*/
